77f9e4966b995e1451d792fc01c1026811ae73fc
Alex Merson <aim@cygnus4.ipac.caltech.edu>
2016-06-14 15:42:14 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='galform',
+      version='0.1',
+      description='User tools for the Galform semi-annalytical model',
+      url='http://bitbucket.org/aimerson/galformtools',
+      author='Alex Merson',
+      author_email='alex.i.merson@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
+
