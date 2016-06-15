4097b307ef082f933cf25972c41d16b513cccff3
Alex Merson <alex.i.merson@gmail.com>
2016-06-14 19:29:53 -0700

    Added galform/io.py with currently empty GalformOutput class.

---
+++ galform/io.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+import numpy as np
+
+
+
+class GalformOutput(object):
+    
+    def __init__(self,outdir):                
+        self.outdir = outdir
+        return
+        
