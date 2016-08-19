c942dc292aea22586a7fc5124231fc87a3f7bdde
Alex Merson <alex.i.merson@gmail.com>
2016-08-19 01:30:55 +0100

    Updated io.py to store cosmology object in GalformHDF5 class.

--- galform/io.py
+++ galform/io.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from .hdf5 import HDF5
 from .GalformError import FileError
-
+from .cosmology import Cosmology
 
 class GalformHDF5(HDF5):
     
@@ -37,6 +37,17 @@ class GalformHDF5(HDF5):
             else:
                 self.parameters[str(param)] = np.copy(value[()])
  
+        # Set cosmology
+        omega0 = self.parameters["omega0"]
+        lambda0 = self.parameters["lambda0"]
+        omegab = self.parameters["omegab"]
+        h0 = self.parameters["h0"]
+        sigma8 = self.parameters["sigma8"]
+        ns = 1
+        self.cosmology = Cosmology(omega0=omega0,lambda0=lambda0,omegab=omegab,\
+                                       h0=h0,sigma8=sigma8,ns=ns)
+
+
         # Store outputs
         nout = np.array(self.fileObj["Output_Times"]["nout"])[()]
         dtype = [("a",float),("z",float),("t",float),("tlbk",float)]
@@ -45,13 +56,27 @@ class GalformHDF5(HDF5):
         self.outputs.z = np.array(self.fileObj["Output_Times"]["zout"])
         self.outputs.t = np.array(self.fileObj["Output_Times"]["tout"])
         self.outputs.tlbk = np.array(self.fileObj["Output_Times"]["tlkbk"])
+
         
+        #print self.fileObj.keys()
+
+               
+
         return
     
 
-    def selectOutput(self,z):
+    def selectOutput(self,z,returnPath=False):
         funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
         iz = np.argmin(np.fabs(self.outputs.z-z))
         outstr = "Output"+str(iz+1).zfill(3)
-        return self.fileObj[outstr]
+        if returnPath:
+            out = outstr
+        else:
+            out = self.fileObj[outstr]
+        return out
 
+
+    def availableProperties(self,z):
+        funcname = self.__class__.__name__+"."+sys._getframe().f_code.co_name
+        out = self.selectOutput(z,returnPath=True)
+        return list(map(str,self.lsDatasets(out)))
